19cd9110b219652c86ccf7223ff84d329d105b72
Mike Charlton <mike.charlton@yourgolftravel.com>
2023-10-02 10:47:01 +0900

    Show that mixin works with both include and extend

--- spec/ff_monads_spec.rb
+++ spec/ff_monads_spec.rb
@@ -4,12 +4,26 @@ require_relative '../lib/ff_monads'
 
 RSpec.describe FFMonads do
   describe 'Mixin with extend' do
-    module Foo
+    module ModuleFoo
       extend FFMonads
     end
 
     it 'does something' do
-      expect(Foo.some(42).to_s).to eql('some(42)')
+      expect(ModuleFoo.some(42).to_s).to eql('some(42)')
+    end
+  end
+
+  describe 'Mixin with include' do
+    class ClassFoo
+      include FFMonads
+
+      def some_forty_two
+        some(42)
+      end
+    end
+
+    it 'does something' do
+      expect(ClassFoo.new.some_forty_two.to_s).to eql('some(42)')
     end
   end
 end
