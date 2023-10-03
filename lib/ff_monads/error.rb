6223fe246d3ae02942101ebaef62c8257630d2a2
Mike Charlton <mike.charlton@yourgolftravel.com>
2023-10-03 09:46:07 +0900

    Add documentation for NoValueError

--- lib/ff_monads/error.rb
+++ lib/ff_monads/error.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 module FFMonads
+  # This exception will get raised when trying to get the value of
+  # a monad that has no value set.  For example `none.v!`.
   class NoValueError < StandardError
     attr_reader :source
 
+    # @param source [FFMonads::Maybe] The monad involved in the error.
     def initialize(source)
       @source = source
       super("Attempt to access unset value in monad: #{source.to_h.inspect}")
