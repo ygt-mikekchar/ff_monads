a9f1eaf6a4b506a7dc4c6588ac6a24a2483fb372
Mike Charlton <mike.charlton@yourgolftravel.com>
2023-10-09 13:38:26 +0900

    Write documentation for top level Maybe class
    Not sure how that's really supposed to be done.  I suppose
    I could have just written examples since Maybe is an abstract
    class.  I don't think Rubyists tend to have abstract classes,
    though.

--- lib/ff_monads/maybe.rb
+++ lib/ff_monads/maybe.rb
@@ -3,7 +3,82 @@
 require_relative 'error'
 
 module FFMonads
+  # Represents an optional value.
+  # Maybe is normally used in conjunction with `.map` or `.and_then`
+  # to operate on an optional value without crashing when the value
+  # does not exist.
+  # @example You can mix in this module.
+  #   include FFMondas::Maybe::Mixin
+  # @example A value that exists.
+  #   some(42)
+  # @example A value that does not exist.
+  #   none
   class Maybe
+    # Applies an optional value to a block.
+    # It takes the value from the Maybe, transforms it,
+    # puts back into a Maybe and returns it.
+    # @param [Block] A block accepting the value and returning a transformed value.
+    # @return [FFMonads::Maybe] The value from the block wrapped in a Maybe.
+    # @example Called with some, it will always return some.
+    #   some(42).map {|x| x + 1} # returns some(43)
+    # @example Called with none, it will always return none.
+    #   none.map {|x| x + 1} # returns none
+    def map(&); end
+
+    # Applies an optional value to a block.
+    # It takes the value from the Maybe, transforms it,
+    # and returns it without putting it back into a Maybe.
+    # It assumes the block will put the value back into
+    # a Maybe (or other monad).
+    # @param [Block] A block accepting and returning a transformed value wrapped in a Monad.
+    # @return [FFMonads::Maybe] Monad returned from the block.
+    # @example `.and_then` allows you to choose the monad to return.
+    #   # This returns none
+    #   some(0).and_then do |x|
+    #     if x == 0
+    #       none
+    #     else
+    #       some(100 / x)
+    #     end
+    #   end
+    def and_then(&); end
+
+    # Returns true if the Maybe is none.
+    # @return [Boolean]
+    def none?; end
+
+    # Returns true if the Maybe is some.
+    # @return [Boolean]
+    def some?; end
+
+    # Returns the value in the Maybe.
+    # Raises NoValueError if the Maybe is none.
+    # @return [Something] the value in the Maybe.
+    # @raise [FFMonads::NoValueError] if the Maybe is none.
+    def v!; end
+
+    # Returns true if this Maybe contains an equivalent value as the other Maybe.
+    # @param [FFMonads::Maybe] other
+    # @return [Boolean]
+    def eql?; end
+
+    # Returns a string representation of the Maybe.
+    # @return [String]
+    # @example Mostly used for #inspect.
+    #   some(42).to_s # returns "some(42)"
+    #   none.to_s     # returns "none"
+    def to_s; end
+
+    # Returns a hash representation of the Maybe.
+    # Possibly for serialisation???
+    # @return [Hash<Symbol, Value>]
+    # @example
+    #   some(42).to_h # returns { type: Some, value: 42 }
+    #   none.to_h     # returns { type: None }
+    def to_h; end
+
+    # Returns a string representation of the Maybe
+    # @see #to_s
     def inspect
       to_s
     end
