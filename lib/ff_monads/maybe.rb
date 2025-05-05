f5990e7eaab7d8951a16c12a777ec28dd76da08e
Mike Charlton <mike.charlton@yourgolftravel.com>
2025-05-05 12:31:24 +0900

    Add type information to Maybe

--- lib/ff_monads/maybe.rb
+++ lib/ff_monads/maybe.rb
@@ -23,6 +23,7 @@ module FFMonads
     #   some(42).map {|x| x + 1} # returns some(43)
     # @example Called with none, it will always return none.
     #   none.map {|x| x + 1} # returns none
+    #: () { [T, U] (T) -> U } -> Maybe[U]
     def map(&block); end
 
     # Applies an optional value to a block.
@@ -41,26 +42,35 @@ module FFMonads
     #       some(100 / x)
     #     end
     #   end
+    #: () { [T, U] (T) -> Maybe[U] } -> Maybe[U]
     def and_then(&block); end
 
     # Returns true if the Maybe is none.
     # @return [Boolean]
+    # () -> bool
     def none?; end
 
     # Returns true if the Maybe is some.
     # @return [Boolean]
+    # () -> bool
     def some?; end
 
     # Returns the value in the Maybe.
     # Raises NoValueError if the Maybe is none.
     # @return [Something] the value in the Maybe.
     # @raise [FFMonads::NoValueError] if the Maybe is none.
+    #: [T] () -> T
     def value!; end
 
     # Returns true if this Maybe contains an equivalent value as the other Maybe.
     # @param other [FFMonads::Maybe] the other Maybe to compare to
     # @return [Boolean]
+    #: (Maybe) -> bool
     def eql?(other); end
+
+    # Returns a string representation of the Maybe
+    #: () -> String
+    def inspect; end
 
     # Returns a hash representation of the Maybe.
     # Possibly for serialisation???
@@ -68,6 +78,7 @@ module FFMonads
     # @example
     #   some(42).to_h # returns { type: Some, value: 42 }
     #   none.to_h     # returns { type: None, value: nil }
+    #: () -> Hash[Symbol, untyped]
     def to_h; end
 
     # Returns a string representation of the Maybe
@@ -94,40 +105,50 @@ module FFMonads
       #   in None
       #     0
       #   end
+      # [T] () -> Array[T]
       def deconstruct
         [@value]
       end
 
+      # () -> bool
       def none?
         false
       end
 
+      # () -> bool
       def some?
         true
       end
 
+      #: () { [T, U] (T) -> U } -> Maybe[U]
       def map(&block)
         self.class.new(block.call(@value))
       end
 
+      #: () { [T, U] (T) -> Maybe[U] } -> Maybe[U]
       def and_then(&block)
         block.call(@value)
       end
 
+      #: [T] () -> T
       def value!
         @value
       end
 
+      #: (Maybe) -> bool
       def eql?(other)
         return false unless other.is_a?(Some)
 
         @value.eql?(other.value!)
       end
 
+      #: () -> String
       def inspect
         "Some(#{@value.inspect})"
       end
 
+      # FIXME: Try to find something better than this
+      #: () -> Hash[Symbol, untyped]
       def to_h
         { type: Some, value: @value }
       end
@@ -144,38 +165,48 @@ module FFMonads
       #   in None
       #     0
       #   end
+      # () -> Array
       def deconstruct
         []
       end
 
+      # () -> bool
       def none?
         true
       end
 
+      # () -> bool
       def some?
         false
       end
 
+      #: () { [T, U] (T) -> U } -> Maybe[U]
       def map(&)
         self
       end
 
+      #: () { [T, U] (T) -> Maybe[U] } -> Maybe[U]
       def and_then(&)
         self
       end
 
+      #: [T] () -> T
       def value!
         raise(NoValueError, self)
       end
 
+      #: (Maybe) -> bool
       def eql?(other)
         other.is_a?(None)
       end
 
+      #: () -> String
       def inspect
         'None()'
       end
 
+      # FIXME: Try to find something better than this
+      #: () -> Hash[Symbol, untyped]
       def to_h
         { type: None, value: nil }
       end
@@ -187,6 +218,7 @@ module FFMonads
     module Mixin
       # Mix in None and Some constants to the current Class/Module.
       # Gives access to None and Some without needing a namespace.
+      #: () -> void
       def add_classes
         const_set('Some', Some)
         const_set('None', None)
@@ -195,12 +227,14 @@ module FFMonads
       # Create a Some containing the value
       # @param value [Something] the value to wrap in a Some
       # @return [Something] The value wrapped in a Some
+      #: [T] (T) -> Some[T]
       def some(value)
         Some.new(value)
       end
 
       # Create a None
       # @return [None]
+      #: () -> None
       def none
         None.new
       end
